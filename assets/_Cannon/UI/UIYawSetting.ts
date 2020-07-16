caf57d148987128591a4c73b0208ae4aa06880ef
ffuh@live.cn <ffuh@live.cn>
2020-07-17 07:58:18 +0800

    0.1F...

--- assets/_Cannon/UI/UIYawSetting.ts
+++ assets/_Cannon/UI/UIYawSetting.ts
@@ -15,21 +15,40 @@ export default class UIYawSetting extends cc.Component {
 
     @property 
     Speed:number =5;
+    @property(cc.Node)
+    AnimTarget:cc.Node;
+
     onLoad()
     {
         this.node.on(cc.Node.EventType.TOUCH_START,this.onMouseDown,this);
         this.node.on(cc.Node.EventType.TOUCH_CANCEL,this.onMouseUp,this);
         this.node.on(cc.Node.EventType.TOUCH_END,this.onMouseUp,this);
     }
+    onEnable()
+    {
+        this.onMouseUp(null);
+    }
     _Touching=false;
     onMouseDown(event)
     {
         this._Touching=true;
+        if(this.AnimTarget!=null)
+        {
+            this.AnimTarget.scale=0.9;
+            this.AnimTarget.color=cc.Color.GRAY;
+        }
+       
 
     }
     onMouseUp(event)
     {
         this._Touching=false;
+        if(this.AnimTarget!=null)
+        {
+            this.AnimTarget.scale=1;
+            this.AnimTarget.color=cc.Color.WHITE;
+        }
+
     }
 
     update(dt)
